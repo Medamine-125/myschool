dca5d65cf98198e60d530563550315f539cfcd6e
Medamine-125 <matouzani12@gmail.com>
2025-04-26 18:35:30 +0200

    n

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #creating the first route
 @app.route("/")
 def index():
-    return "Salut mes amis !"
+    return f'Homepage'
 
 if __name__ == "__main__":
     app.run(debug=True)
